5906d95ab710cb374950ff18ecf4b11fe777cb7b
Elkin <ersantis@uninorte.edu.co>
2024-05-19 17:47:43 -0500

    Refactor eventHandlers.js to improve mode handling and event listeners

--- modes/modes.js
+++ modes/modes.js
@@ -1,6 +1,11 @@
-import {startDrawing,endDrawing,drawCanvas} from "../modes/drawing.js"
-import {startDragging,endDragging,dragCanvas} from "../modes/dragging.js"
-import { handleClickPin, handleClickDelete } from "../handlers/modesHandlers.js";
+import {startDrawing,
+        endDrawing,drawCanvas} from "../modes/drawing.js"
+import {startDragging,
+        endDragging,
+        dragCanvas} from "../modes/dragging.js"
+import { handleClickPin,
+        handleClickDelete,
+        handleClickGate } from "../handlers/modesHandlers.js";
 
 export const eventHandlers = {
     wireMode: {
@@ -13,7 +18,7 @@ export const eventHandlers = {
             mousedown: startDragging,
             mouseup: endDragging,
             mousemove: dragCanvas,
-            click: handleClickPin
+            click: null // Se eliminarán los listeners específicos más adelante
         }
     },
     handMode: {
@@ -26,38 +31,36 @@ export const eventHandlers = {
             mousedown: startDrawing,
             mouseup: endDrawing,
             mousemove: drawCanvas,
-            click: handleClickPin
+            click: null // Se eliminarán los listeners específicos más adelante
         }
     },
     constan01Mode: {
-        add: {
-            click: handleClickPin
-        },
+        add: {},
         remove: {
             mousedown: startDragging,
             mouseup: endDragging,
             mousemove: dragCanvas,
             mousedown: startDrawing,
             mouseup: endDrawing,
-            mousemove: drawCanvas
+            mousemove: drawCanvas,
+            click: null // Se eliminarán los listeners específicos más adelante
         }
     },
     constan00Mode: {
-        add: {
-            click: handleClickPin
-        },
+        add: {},
         remove: {
             mousedown: startDragging,
             mouseup: endDragging,
             mousemove: dragCanvas,
             mousedown: startDrawing,
             mouseup: endDrawing,
-            mousemove: drawCanvas
+            mousemove: drawCanvas,
+            click: null // Se eliminarán los listeners específicos más adelante
         }
     },
     deleteMode: {
         add: {
-            click: handleClickDelete
+            click: (event) => handleClickDelete(event)
         },
         remove: {
             mousedown: startDragging,
@@ -65,20 +68,74 @@ export const eventHandlers = {
             mousemove: dragCanvas,
             mousedown: startDrawing,
             mouseup: endDrawing,
-            mousemove: drawCanvas
+            mousemove: drawCanvas,
+            click: null // Se eliminarán los listeners específicos más adelante
         }
     },
     probeMode: {
-        add: {
-            click: handleClickPin
-        },
+        add: {},
         remove: {
             mousedown: startDragging,
             mouseup: endDragging,
             mousemove: dragCanvas,
             mousedown: startDrawing,
             mouseup: endDrawing,
-            mousemove: drawCanvas
+            mousemove: drawCanvas,
+            click: null // Se eliminarán los listeners específicos más adelante
+        }
+    },
+    andMode: {
+        add: {},
+        remove: {
+            mousedown: startDragging,
+            mouseup: endDragging,
+            mousemove: dragCanvas,
+            mousedown: startDrawing,
+            mouseup: endDrawing,
+            mousemove: drawCanvas,
+            click: null // Se eliminarán los listeners específicos más adelante
         }
     }
+
 };
+
+let handleClickPinListeners = [];
+let handleClickDeleteListeners = [];
+let handleClickGateListeners = [];
+
+export function addEventListenerWithParam(eventType, param) {
+    const wrappedHandler = (event) => handleClickPin(event, param);
+    handleClickPinListeners.push({ eventType, wrappedHandler });
+    canvasContainer.addEventListener(eventType, wrappedHandler);
+}
+
+export function removeEventListenersWithParam() {
+    handleClickPinListeners.forEach(({ eventType, wrappedHandler }) => {
+        canvasContainer.removeEventListener(eventType, wrappedHandler);
+    });
+    handleClickPinListeners = [];
+}
+
+export function addEventListenerWithDelete(eventType) {
+    handleClickDeleteListeners.push({ eventType, handleClickDelete });
+    canvasContainer.addEventListener(eventType, handleClickDelete);
+}
+
+export function removeEventListenersWithDelete() {
+    handleClickDeleteListeners.forEach(({ eventType }) => {
+        canvasContainer.removeEventListener(eventType, handleClickDelete);
+    });
+    handleClickDeleteListeners = [];
+}
+export function addEventListenerWithGate(eventType,param){
+    const wrappedHandler = (event) => handleClickGate(event, param);
+    handleClickGateListeners.push({eventType,wrappedHandler});
+    canvasContainer.addEventListener(eventType,wrappedHandler);
+}
+export function removeEventListenersWithGate(){
+    handleClickGateListeners.forEach(({eventType,wrappedHandler})=>{
+        canvasContainer.removeEventListener(eventType,wrappedHandler);
+    });
+    handleClickGateListeners = [];
+}
+
