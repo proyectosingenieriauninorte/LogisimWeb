2d99554f24ca3eb72ff72903cd1014b23755c3b0
Elkin <ersantis@uninorte.edu.co>
2024-05-19 00:24:38 -0500

    Refactor Line class to include an intermediate point

--- components/line.js
+++ components/line.js
@@ -36,39 +36,4 @@ class Line {
 	}
 }
 
-// Función para verificar si los extremos de una línea están conectados a otra línea
-// export function areEndsConnectedToOtherLine(line, lines) {
-// 	// Arreglo para almacenar las líneas conectadas
-// 	var connectedLines = [];
-
-// 	// Verificar si el inicio de la línea está conectado a otra línea
-// 	for (var i = 0; i < lines.length; i++) {
-// 		if (
-// 			lines[i] !== line &&
-// 			(lines[i].startNode === line.startNode ||
-// 				lines[i].startNode === line.endNode)
-// 		) {
-// 			connectedLines.push(lines[i]);
-// 		}
-// 	}
-
-// 	// Verificar si el final de la línea está conectado a otra línea
-// 	for (var i = 0; i < lines.length; i++) {
-// 		if (
-// 			lines[i] !== line &&
-// 			(lines[i].endNode === line.startNode ||
-// 				lines[i].endNode === line.endNode)
-// 		) {
-// 			connectedLines.push(lines[i]);
-// 		}
-// 	}
-
-// 	// Si hay líneas conectadas, devolver el arreglo
-// 	if (connectedLines.length > 0) {
-// 		return connectedLines;
-// 	} else {
-// 		return null; // Si no hay líneas conectadas, devolver null
-// 	}
-// }
-
 export default Line;
