2e86adc6acc90deccb4376b2f47786d08d6e75b9
Luisangel Parra <lfaria@uninorte.edu.co>
2024-06-04 21:36:40 -0500

    feat: Add intermediate point to Line.js and refactor related files

--- components/Line.js
+++ components/Line.js
@@ -1,9 +1,10 @@
 class Line {
-	constructor(pointA, pointB) {
+	constructor(pointA,pointinter,pointB) {
 		this.lineWidth = 4; // Grosor de la linea
 
 		// Puntos extremos de la linea
 		this.pointA = pointA;
+		this.pointinter = pointinter;
 		this.pointB = pointB;
 	}
 
@@ -11,7 +12,7 @@ class Line {
 	isEqualTo(line) {
 		if (!line instanceof Line) return false;
 
-		let [pA, pB] = line.getPoints();
+		let [pA,pI, pB] = line.getPoints();
 
 		if (
 			(pA.isEqualTo(this.pointA) && pB.isEqualTo(this.pointB)) ||
@@ -29,43 +30,8 @@ class Line {
 
 	// Retorna los puntos extremos de la linea
 	getPoints() {
-		return [this.pointA, this.pointB];
+		return [this.pointA, this.pointinter, this.pointB];
 	}
 }
-
-// Función para verificar si los extremos de una línea están conectados a otra línea
-// export function areEndsConnectedToOtherLine(line, lines) {
-// 	// Arreglo para almacenar las líneas conectadas
-// 	var connectedLines = [];
-
-// 	// Verificar si el inicio de la línea está conectado a otra línea
-// 	for (var i = 0; i < lines.length; i++) {
-// 		if (
-// 			lines[i] !== line &&
-// 			(lines[i].startNode === line.startNode ||
-// 				lines[i].startNode === line.endNode)
-// 		) {
-// 			connectedLines.push(lines[i]);
-// 		}
-// 	}
-
-// 	// Verificar si el final de la línea está conectado a otra línea
-// 	for (var i = 0; i < lines.length; i++) {
-// 		if (
-// 			lines[i] !== line &&
-// 			(lines[i].endNode === line.startNode ||
-// 				lines[i].endNode === line.endNode)
-// 		) {
-// 			connectedLines.push(lines[i]);
-// 		}
-// 	}
-
-// 	// Si hay líneas conectadas, devolver el arreglo
-// 	if (connectedLines.length > 0) {
-// 		return connectedLines;
-// 	} else {
-// 		return null; // Si no hay líneas conectadas, devolver null
-// 	}
-// }
 
 export default Line;
